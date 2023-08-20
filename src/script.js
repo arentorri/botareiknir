ad73cf88cb26ffe5ea77dee45a4955e709a26ff5
Sara Þórhallsdóttir <sarathorhalls@gmail.com>
2023-08-20 23:56:40 +0000

    retrives visitala from hagstofan

--- src/script.js
+++ src/script.js
@@ -2,9 +2,10 @@ import { getLansKjaraVisitala } from "./lanskjaravisitala.js";
 import { athugaKennitolu } from "./athugaKennitolur.js";
 import { calculateAgeAtDate, calculateMargfeldisstudull, calculateMiskabotValue } from "./calculations.js";
 
-// getLansKjaraVisitala();
+// Handle submit
+document.getElementById('calc-form').addEventListener('submit',handleSubmit);
 
-document.getElementById('calc-form').addEventListener('submit', function (event) {
+async function handleSubmit(event) {
     event.preventDefault();
 
     const lawyerEmail = document.getElementById('lawyerEmail').value;
@@ -32,6 +33,11 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
     const accidentYear = accidentDateComponents[0];
     const formattedAccidentDate = `${accidentDay}/${accidentMonth}/${accidentYear}`;
 
+    // Calculate age at the time of the accident
+    const birthDate = new Date(year, month - 1, day); // Construct birthDate from dob components
+    const accidentDate = new Date(accidentYear, accidentMonth - 1, accidentDay); // Construct accidentDate from accidentDate components
+    const ageAtAccident = calculateAgeAtDate(birthDate, accidentDate);
+
     //Checkboxin - fastar
     const compensationCheckbox = document.getElementById('compensationCheckbox').checked;
     const employmentLossCheckbox = document.getElementById('employmentLossCheckbox').checked;
@@ -39,18 +45,25 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
     const varanlegOrorkaCheckbox = document.getElementById('varanlegOrorkaCheckbox').checked;
 
     // Compensation calculation
+    // Get LansKjaraVisitala from Hagstofa
+    let lanskjaravisitala = 0; // Fallback value
+    await getLansKjaraVisitala(accidentDate).then(json => {
+        lanskjaravisitala = parseFloat(json.data[0].values[0]) / 3282;
+    }).catch(err => {
+        console.log(err);
+    });
+
+    // Calculate the compensation for sick days
     const sickDaysWithAccommodation = parseInt(document.getElementById('sickDaysWithAccommodation').value) || 0;
     const sickDaysWithoutAccommodation = parseInt(document.getElementById('sickDaysWithoutAccommodation').value) || 0;
-    const loanInterestRate = parseFloat(document.getElementById('loanInterestRate').value) || 0;
-    const uppreiknadBeturMeðRúmlegu = Math.round((1300 * (loanInterestRate / 3282)) /10 ) * 10;
-    const uppreiknadBeturÁnRúmlegu = Math.round((700 * (loanInterestRate / 3282)) / 10) * 10;
+    const uppreiknadBeturMeðRúmlegu = Math.round((1300 * lanskjaravisitala) /10 ) * 10;
+    const uppreiknadBeturÁnRúmlegu = Math.round((700 * lanskjaravisitala) / 10) * 10;
     const heildarBeturMeðRúmlegu = uppreiknadBeturMeðRúmlegu * sickDaysWithAccommodation;
     const heildarBeturÁnRúmlegu = uppreiknadBeturÁnRúmlegu * sickDaysWithoutAccommodation;
     const heildarBótakrafaFyrirÞjáningabætur = heildarBeturMeðRúmlegu + heildarBeturÁnRúmlegu;
 
     let results = {};
 
-    let compensationResults = '';
     if (compensationCheckbox) {
         results = {...results, compensationResults: {
             sickDaysWithAccommodation: sickDaysWithAccommodation,
@@ -59,22 +72,12 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
             uppreiknadBeturÁnRúmlegu: uppreiknadBeturÁnRúmlegu,
             heildarBeturMeðRúmlegu: heildarBeturMeðRúmlegu,
             heildarBeturÁnRúmlegu: heildarBeturÁnRúmlegu,
-            heildarBótakrafaFyrirÞjáningabætur: heildarBótakrafaFyrirÞjáningabætur
-        }}
-        compensationResults = `
-            <h3>Þjáningabætur</h3>
-            <p><strong>Fjöldi daga veikinda með rúmlegu:</strong> ${sickDaysWithAccommodation}</p>
-            <p><strong>Fjöldi daga veikinda án rúmlegu:</strong> ${sickDaysWithoutAccommodation}</p>
-            <p><strong>Uppreiknaðar bætur fyrir veikindi með rúmlegu:</strong> ${uppreiknadBeturMeðRúmlegu}</p>
-            <p><strong>Uppreiknaðar bætur fyrir veikindi án rúmlegu:</strong> ${uppreiknadBeturÁnRúmlegu}</p>
-            <p><strong>Heildarbætur fyrir veikindi með rúmlegu:</strong> ${heildarBeturMeðRúmlegu}</p>
-            <p><strong>Heildarbætur fyrir veikindi án rúmlegu:</strong> ${heildarBeturÁnRúmlegu}</p>
-            <p><strong>Heildarbótakrafa fyrir þjáningabætur:</strong> ${heildarBótakrafaFyrirÞjáningabætur}</p>
-        `;
+            heildarBótakrafaFyrirÞjáningabætur: heildarBótakrafaFyrirÞjáningabætur,
+            lanskjaravisitala: lanskjaravisitala
+        }};
     }
 
     // Tímabundið atvinnutjón calculation
-    let employmentLossResults = '';
     if (employmentLossCheckbox) {
         const disabilityMonths = parseInt(document.getElementById('disabilityMonths').value) || 0;
         const monthlyIncome = parseFloat(document.getElementById('monthlyIncome').value) || 0;
@@ -101,35 +104,16 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
             totalDisabilityCompensation: totalDisabilityCompensation,
             totalBenefits: totalBenefits,
             totalCompensationAfterBenefits: totalCompensationAfterBenefits
-        }}
-        employmentLossResults = `
-            <h3>Tímabundið atvinnutjón</h3>
-            <p><strong>Fjöldi mánaða óvinnufærni:</strong> ${disabilityMonths}</p>
-            <p><strong>Mánaðartekjur:</strong> ${monthlyIncome}</p>
-            <p><strong>Lífeyrismótframlag vinnuveitenda:</strong> ${employerPensionContribution}%</p>
-            <p><strong>Dagpeningar frá lífeyrissjóði:</strong> ${pensionFundPayments}</p>
-            <p><strong>Greiðsla frá vinnuveitanda:</strong> ${employerPayments}</p>
-            <p><strong>Greiðsla frá sjúkrasjóði:</strong> ${sicknessFundPayments}</p>
-            <p><strong>Aðrar bætur frá opinberum tryggingum:</strong> ${otherBenefits}</p>
-            <p><strong>Vátryggingarbætur sem eru raunveruleg skaðabót og sambærilegar greiðslur:</strong> ${insuranceBenefits}</p>
-            <p><strong>Heildarlaun fyrir tíma óvinnufærni:</strong> ${totalDisabilityCompensation}</p>
-            <p><strong>Samtals frádráttur:</strong> ${totalBenefits}</p>
-            <p><strong>Heildarbótakrafa fyrir tímabundið atvinnutjón:</strong> ${totalCompensationAfterBenefits}</p>
-        `;
+        }};
     }
     
     // Varanlegur miski calculation
-    let permanentLossResults = '';
     if (permanentLossCheckbox) {
-        const birthDate = new Date(year, month - 1, day); // Construct birthDate from dob components
-        const accidentDate = new Date(accidentYear, accidentMonth - 1, accidentDay); // Construct accidentDate from accidentDate components
-        const ageAtAccident = calculateAgeAtDate(birthDate, accidentDate);
-
         const permanentLossLevel = parseFloat(document.getElementById('permanentLossLevel').value) || 0;
         const miskabotValue = calculateMiskabotValue(ageAtAccident);
 
         // Calculate Miskabótagrundvöllur uppreiknaður sbr. 15. gr. skbl.
-        const miskabotUppreiknadur = Math.round(miskabotValue * (loanInterestRate / 3282));
+        const miskabotUppreiknadur = Math.round(miskabotValue * lanskjaravisitala);
 
         // Calculate Miskabótagrundvöllur uppreiknaður og námundaður, sbr. 1. mgr. 15. gr. skbl.
         const miskabotUppreiknadurNamundadur = Math.round(miskabotUppreiknadur / 500) * 500;
@@ -143,15 +127,7 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
             miskabotValue: miskabotValue,
             miskabotUppreiknadurNamundadur: miskabotUppreiknadurNamundadur,
             heildarkrafaBotaVegnaVaranlegsMiska: heildarkrafaBotaVegnaVaranlegsMiska
-        }}
-        permanentLossResults = `
-            <h3>Varanlegur miski</h3>
-            <p><strong>Miskastig:</strong> ${permanentLossLevel}%</p>
-            <p><strong>Aldur tjónþola á tjóndegi:</strong> ${ageAtAccident.years} ára og ${ageAtAccident.days} daga</p>
-            <p><strong>Miskabótagrundvöllur m.v. aldur tjónþola á tjóndegi:</strong> ${miskabotValue}</p>
-            <p><strong>Miskabótagrundvöllur uppreiknaður og námundaður, sbr. 1. mgr. 15. gr. skbl.:</strong> ${miskabotUppreiknadurNamundadur.toFixed(0)}</p>
-            <p><strong>Heildarbótakrafa fyrir tímabundið atvinnutjón:</strong> ${heildarkrafaBotaVegnaVaranlegsMiska}</p>
-        `;
+        }};
     }
 
     //Varanleg örorka    
@@ -165,24 +141,18 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
     const wageIndex3YearBeforeClaim = parseFloat(document.getElementById('wageIndex3YearsBeforeClaim').value) || 0;
     const disabilityLevel = parseFloat(document.getElementById('disabilityLevel').value) || 0;
 
-    // Calculate age at the time of the accident
-    const birthDate = new Date(year, month - 1, day); // Construct birthDate from dob components
-    const accidentDate = new Date(accidentYear, accidentMonth - 1, accidentDay); // Construct accidentDate from accidentDate components
-    const ageAtAccident = calculateAgeAtDate(birthDate, accidentDate);
-
     // Calculate the margfeldisstuðull for the age at the time of the accident
     const margfeldisstudull = calculateMargfeldisstudull(ageAtAccident); // Pass the ageAtAccident object
 
-    let permanentDisabilityResults = '';
     if (varanlegOrorkaCheckbox) {
 
         // Lágmark bótagrundvöllur uppreiknaður
-        const lagmarkBotagrundvollurUppreiknadur = Math.round(1200000 * (loanInterestRate / 3282));
+        const lagmarkBotagrundvollurUppreiknadur = Math.round(1200000 * lanskjaravisitala);
 
         // Námundun, heill og hálfu þúsundi, skv. 1. mgr. 15. gr. skbl.:
 
         // Hámark bótagrundvöllds, uppreiknað:
-        const hamarkBotagrundvollurUppreiknadur = Math.round(4500000 * (loanInterestRate / 3282));
+        const hamarkBotagrundvollurUppreiknadur = Math.round(4500000 * lanskjaravisitala);
 
         // Námundun, heill og hálfu þúsundi, skv. 1. mgr. 15. gr. skbl.
 
@@ -230,20 +200,6 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
             disabilityLevel: disabilityLevel,
             heildaraKrafaFyrirFradratt: heildaraKrafaFyrirFradratt
         } };
-        permanentDisabilityResults = `
-            <h3>Varanleg örorka</h3>
-            <p><strong>Lágmark bótagrundvölls, uppreiknað:</strong> ${lagmarkBotagrundvollurUppreiknadur}</p>
-            <p><strong>Hámark bótagrundvölls, uppreiknað:</strong> ${hamarkBotagrundvollurUppreiknadur}</p>
-            <p><strong>Tekjur 1 ár fyrir tjón, uppreiknaðar:</strong> ${tekjur1ArFyrirUppreiknad}</p>
-            <p><strong>Tekjur 2 ár fyrir tjón, uppreiknaðar:</strong> ${tekjur2ArFyrirUppreiknad}</p>
-            <p><strong>Tekjur 3 ár fyrir tjón, uppreiknaðar:</strong> ${tekjur3ArFyrirUppreiknad}</p>
-            <p><strong>Meðaltal tekna:</strong> ${medaltalTekna}</p>
-            <p><strong>Lífeyrismótframlag vinnuveitanda:</strong> ${lifeyrismotframlagVinnuveitanda}</p>
-            <P><strong>Árslaunaviðmið </strong>(Meðaltal tekna + lífeyrismótframlag): ${medaltalOgLifeyrir}</p>
-            <p><strong>Örorka:</strong> ${disabilityLevel}</p>
-            <p><strong>Heildarkrafa fyrir varanlega Örorku fyrir frádrátt</strong> ${heildaraKrafaFyrirFradratt}
-
-        `;
     }
 
     results = {...results, lawyerEmail: lawyerEmail };
@@ -259,4 +215,4 @@ document.getElementById('calc-form').addEventListener('submit', function (event)
     // Open new window with data
     const resultsWindow = window.open("../public/results.html", "_blank");
     resultsWindow.focus();
-});
+};
